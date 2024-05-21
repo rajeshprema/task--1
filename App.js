95db0e736b11f576f201fa4b298e53bb616d5747
rajeshprema <122607002+rajeshprema@users.noreply.github.com>
2024-05-21 07:03:49 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
